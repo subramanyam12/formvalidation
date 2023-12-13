15ce61e817a6d4e9f07a8fcb34889393b5ef6224
subramanayam12 <subramanyam933@gmail.com>
2023-12-13 10:39:50 +0530

    adding changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { Store } from './redux/Store.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={Store}>
          <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
 
   </React.StrictMode>,
 )
